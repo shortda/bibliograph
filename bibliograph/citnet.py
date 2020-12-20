67140ab79e68321edc30fc2aee8c96f462e21b93
Devin <short.devin@gmail.com>
2020-12-20 14:16:18 -0800

    refactored as pd.Dataframe wrapper

--- bibliograph/citnet.py
+++ bibliograph/citnet.py
@@ -1,250 +1,287 @@
-import json
-import networkx as nx
 import pandas as pd
-from .readwrite import slurpBibTex
-from .readwrite import slurpReferenceCSV
+import networkx as nx
+from .nasaads import queryads
+from .nasaads import query_ads_bibcodes
+from .readwrite import slurp_bibtex
+from .readwrite import slurp_csv
 from .util import backup
-from .util import bibUpdate 
-from .util import makeGraph
-from .nasaads import queryADSbibcodes
-from .nasaads import queryADS
-from os.path import isfile
+from .util import updater
 
-class citnet:
-	'''
-	The citnet class provides a citation network object that contains
-	a pandas DataFrame for a bibliography, a DataFrame for references
-	between bibliography, and a NetworkX graph representing the
-	network. 
 
-	Parameters
-	----------
-	name : string
-		prefix 
+class CitNet:
+    '''
+    The citnet class provides a citation network object that contains
+    a pandas DataFrame for a bibliography, a DataFrame for references
+    between bibliography, and a NetworkX graph representing the
+    network.
 
-	bibtex : string
-		Name of BibTex file containing the bibliography
+    Parameters
+    ----------
+    bibcols : list-like
+        List of labels for bibliography columns. Passed directly to
+        pd.Dataframe constructor as 'columns' keyword argument.
+        Required when initializing from bibtex.
 
-	bibcols : list-like
-		List of labels for bibliography columns
+    refcols : list-like
+        Labels of columns whose values should be joined by spaces to
+        create a unique reference string for each row. Required when
+        initializing from bibtex.
 
-	refcols : list-like OR string
-		Labels of columns whose values should be joined by spaces to
-		create a unique reference string for each row. If string, must
-		contain a column label. Defaults to 'title'.
-	
-	bibTex_processors : dictionary
-		tag_processors is a dictionary with format 
-		
-			{bibTexTag:[columnName, function_to_process_bibTex]}
-		or
-			{bibTexTag:[[columnName1, function1_to_process_bibTex],
-						[columnName2, function2_to_process_bibTex]]}
+    citcols : list-like
+        Optional additional columns to store metadata for citation
+        network edges.
 
-		where bibTexTag is a field in the BibTex entries, columnName
-		is the label for a bibliography column where data from that
-		tag should be stored, and the functions translate the text of
-		the BibTex entry into a value that should be stored in the 
-		bibliography column
+    bibtex : string
+        Name of BibTex file containing the bibliography
 
-	'''
-	# TODO : make abbr an attribute of the citation network?
-	def __init__(self, data=None, index=None, bibcols=None, bibtex=None, csv=None, fileprefix=None, refcols='title', bibTex_processors=None, direction='outgoing', uid='ref', noNewSources=False, separator=' | ', translator=None):
+    data : ndarray, Iterable, dict, or DataFrame
+        Optional data to initialize bibliography. Passed directly to
+        pd.DataFrame constructor.
 
-		self.bib = pd.DataFrame(data=data, index=index, columns=bibcols, dtype=str)
-		self.cit = pd.DataFrame(columns=['src', 'tgt'], dtype='int')
+    index : Index or array-like
+        Optional index to initialize bibliography. Passed directly to
+        pd.DataFrame constructor.
 
-		self.refcols = refcols
-		if type(refcols) == str:
-			self.uid = refcols
-		else:
-			self.uid = 'ref'
+    bibtex : string
+        Name of BibTex file containing data for the bibliography.
 
-		if bibtex is not None:
-			if (csv is not None) or (fileprefix is not None):
-				raise ValueError('citnet is initialized with exactly one of bibtex, csv, or fileprefix. Got at values for at least two.')
-			slurpBibTex(self, bibtex, bibcols=bibcols, refcols=refcols, tag_processors=bibTex_processors)
+    csv : string
+        Name of csv file containing sources and targets. If bibcols
+        and refcols are None, this function reads bibcols and refcols
+        from the first two lines of the csv file.
 
-			self.notUnique = [c for c in self.bib if c != self.uid]
+    fileprefix : string
+        Prefix of files containing saved CitNet data.
 
-			self.bib = self.bib.fillna('x')
-			self.graph = makeGraph(self.bib, self.cit, self.uid)
+    bibtex_parsers : dictionary
+        bibtex_parsers is a dictionary with format
 
-		if csv is not None:
-			if (bibtex is not None) or (fileprefix is not None):
-				raise ValueError('citnet is initialized with exactly one of bibtex, csv, or fileprefix. Got at values for at least two.')
-			print('Loading data from ' + csv)
-			slurpReferenceCSV(self, csv, direction, noNewSources, separator, translator)
+            {tag:[col, function_to_process_bibtex]}
+        or
+            {tag:[[col1, function1_to_process_bibtex],
+                  [col2, function2_to_process_bibtex]]}
 
-		if fileprefix is not None:
-			if (bibtex is not None) or (csv is not None):
-				raise ValueError('citnet is initialized with exactly one of bibtex, csv, or fileprefix. Got at values for at least two.')
-			checkBib = isfile(fileprefix + '-bib.json')
-			checkCit = isfile(fileprefix + '-cit.json')
-			checkGraph = isfile(fileprefix + '.graphml')
+        where tag is a field in the BibTex entries, col
+        is the label for a bibliography column where data from that
+        tag should be stored, and the functions translate the text of
+        the BibTex entry into a value that should be stored in the
+        bibliography column.
+    '''
 
-			if all([checkBib, checkCit, checkGraph]):
-				self.bib = pd.read_json(fileprefix + '-bib.json')
-				self.cit = pd.read_json(fileprefix + '-cit.json')
-				self.graph = nx.read_graphml(fileprefix + '.graphml')
-				self.notUnique = [c for c in self.bib if c != self.uid]
-				print('\nNetwork loaded from disk.\n')
-			else:
-				raise RuntimeError('\nFound at least one stored file for bib, cit, or graph, but did not find all three.')
+    def __init__(self, **kwargs):
 
-	def update(self, newEntry, updateCit=False, src=None):
-		'''
-		Take data for a bibliography entry and either overwrite an
-		existing entry with new data, rewrite the same entry, or add
-		a new entry to the bibliography.
+        if (('refcols' not in kwargs) or ('bibcols' not in kwargs)) and ('bibtex' in kwargs):
+            raise ValueError('bibcols and refcols required when initializing from bibtex.')
+        if ('bibcols' in kwargs):
+            bibcols = kwargs['bibcols']
+            if 'ref' not in bibcols:
+                bibcols = bibcols + ['ref']
 
-		Parameters
-		----------
-		newEntry : pd.Series or pd.DataFrame
-			either a pandas Series object or a DataFrame containing a
-			single row that can be squeezed into a Series. Contains
-			data for a bibliography entry that may or may not already
-			exist in the bibliography DataFrame.
+        dfargs = ['data', 'index', 'columns']
+        self.bib = pd.DataFrame(dtype='str', **{arg:kwargs[arg] for arg in dfargs if arg in kwargs})
+        if 'citcols' in kwargs:
+            self.cit = pd.DataFrame(columns=(['src', 'tgt'] + kwargs['citcols']))
+        else:
+            self.cit = pd.DataFrame(columns=['src', 'tgt'])
 
-		updateCit : boolean
-			If True, create new citation edge for this target. 
+        if 'bibtex' in kwargs:
+            if ('csv' in kwargs) or ('fileprefix' in kwargs):
+                raise ValueError('citnet is initialized with exactly one of bibtex, csv, or fileprefix. Got values for at least two.')
+            bibtex = kwargs['bibtex']
+            if 'refcols' in kwargs:
+                self.refcols = kwargs['refcols']
+            else:
+                self.refcols = 'title'
+            bibtexargs = ['bibcols', 'bibtex_parsers']
+            print('\nLoading data from ' + bibtex + '\n')
+            slurp_bibtex(self, bibtex, self.refcols, **{arg:kwargs[arg] for arg in bibtexargs if arg in kwargs})
+            self.bib = self.bib.fillna('x')
 
-		src : integer
-			Bibliography index of source that references this target.
-		'''
-		# TODO : make this function update the graph?
+        if 'csv' in kwargs:
+            if ('bibtex' in kwargs) or ('fileprefix' in kwargs):
+                raise ValueError('citnet is initialized with exactly one of bibtex, csv, or fileprefix. Got values for at least two.')
+            csv = kwargs['csv']
+            csvargs = ['direction', 'sources_from_csv', 'csv_separator', 'csv_parser']
+            print('\nLoading data from ' + csv + '\n')
+            slurp_csv(self, csv, **{arg:kwargs[arg] for arg in csvargs if arg in kwargs})
 
-		getUpdate = bibUpdate(self.bib, newEntry, self.uid)
+        if 'fileprefix' in kwargs:
+            if ('bibtex' in kwargs) or ('csv' in kwargs):
+                raise ValueError('citnet is initialized with exactly one of bibtex, csv, or fileprefix. Got values for at least two.')
+            fileprefix = kwargs['fileprefix']
+            self.bib = pd.read_json(fileprefix + '-bib.json')
+            try:
+                self.cit = pd.read_json(fileprefix + '-cit.json')
+            except FileNotFoundError:
+                print(fileprefix + '-cit.json not found, creating blank citation dataframe.')
+            try:
+                self.graph = nx.read_graphml(fileprefix + '.graphml')
+            except FileNotFoundError:
+                print(fileprefix + '.graphml not found, creating blank graph.')
+                print('NOT IMPLEMENTED')
 
-		if getUpdate.updated:
-			self.bib.loc[getUpdate.index] = getUpdate.entry
-			if updateCit and not ((self.cit.src == src) & (self.cit.tgt == getUpdate.index)).any():
-				self.cit = self.cit.append({'src':src, 'tgt':getUpdate.index}, ignore_index=True)
-		else:
-			self.bib = self.bib.append(newEntry, ignore_index=True).fillna('x')
-			if updateCit:
-				self.cit = self.cit.append({'src':src, 'tgt':self.bib.index[-1]}, ignore_index=True)
-	
-	def loadCSV(self, filename, **kwargs):
-		'''
-		Get bibliography and citation data from a csv file.
+    def __getattr__(self, attr):
 
-		Parameters
-		----------
-		filename : string
-			Name of csv file.
+        if attr in self.__dict__:
+            return getattr(self, attr)
+        return getattr(self.bib, attr)
 
-		kwargs
-			Keyword arguments passed to
-			bibliography.readwrite.slurpReferenceCSV
-		'''
-		print('Loading data from ' + filename)
-		slurpReferenceCSV(self, filename, **kwargs)
+    def __getitem__(self, item):
 
-	def writeNetwork(self, name):
-		'''
-		Write JSON files representing the bibliography and citation
-		DataFrames. Write a graphml file representing the graph.
+        return self.bib[item]
 
-		Parameters
-		----------
-		name : string
-			Network name will be the prefix for all stored filenames.
-		'''
+    def __setitem__(self, item, data):
 
-		backup(name + '-bib.json')
-		self.bib.to_json(name + '-bib.json')
+        self.bib[item] = data
 
-		backup(name + '-cit.json')
-		self.cit.to_json(name + '-cit.json')
+    def append(self, *args, **kwargs):
+        '''
+        Wraps pd.DataFrame.append() so append on CitNet appends to the
+        bib DataFrame.
+        '''
+        self.bib = self.bib.append(*args, **kwargs)
 
-		if self.graph:
-			backup(name + '.graphml')
-			nx.write_graphml(self.graph, name + '.graphml')
+    def update_entry(self, entry, update_citations=False, src=None):
+        '''
+        Take data for a bibliography entry and either overwrite an
+        existing entry with new data, rewrite the same entry, or add
+        a new entry to the bibliography.
 
-	def getADSbibcodes(self, searchColumns, **kwargs):
-		'''
-		Get ADS bibcodes for papers in the sources DataFrame. If this
-		function tries to construct a query for some entry but can't
-		(the index for that entry winds up in badQueries), the
-		bibcode field for that entry gets '?'.
+        Parameters
+        ----------
+        entry : pd.Series or pd.DataFrame
+            either a pandas Series object or a DataFrame containing a
+            single row that can be squeezed into a Series. Contains
+            data for a bibliography entry that may or may not already
+            exist in the bibliography DataFrame.
 
-		Parameters
-		----------
-		searchColumns : list-like
-			Column labels in the sources DataFrame which contain data
-			for the ADS search queries.
+        update_citations : boolean
+            If True, create new citation edge for this target. 
 
-		kwargs
-			Keyword arguments are passed directly to
-			bibliograph.nasaads.queryADSbibcodes
+        src : integer
+            Bibliography index of source that references this target.
+        '''
+        result = updater(self.bib, entry)
 
-		Returns
-		-------
-		queries : pd.DataFrame
-			pandas DataFrame with query strings, ads articles objects 
-			retreived from the ADS, and bibcodes from those articles
-			objects. queries index corresponds to sources index.
+        if result.updated:
+            self.bib.loc[result.index] = result.entry
+            if update_citations and not ((self.cit.src == src) & (self.cit.tgt == result.index)).any():
+                self.cit = self.cit.append({'src':src, 'tgt':self.bib.index[-1]}, ignore_index=True)
 
-		badQueries : list
-			List of index values from the sources DataFrame for which
-			values in columns to be searched either contained spaces
-			or were 'x'.	
-		'''
-		queries, badQueries = queryADSbibcodes(self.bib, searchColumns, **kwargs)
+    def load_reference_csv(self, csv, **kwargs):
+        '''
+        Get bibliography and citation data from a csv file.
 
-		self.bib.loc[queries.index, 'bibcode'] = queries['bibcode']
-		self.bib.loc[badQueries, 'bibcode'] = '?'
+        Parameters
+        ----------
+        csv : string
+            Name of csv file.
 
-		return(queries, badQueries)
+        kwargs
+            Keyword arguments passed to
+            bibliography.readwrite.slurp_csv
+        '''
+        print('\nLoading data from ' + csv + '\n')
+        slurp_csv(self, csv, **kwargs)
 
-	def queryADS(self, searchColumns, fetchTerms, **kwargs):
-		'''
-		Get ADS data for bibliography entries.
+    def save_citnet(self, name):
+        '''
+        Write JSON files representing the bibliography and citation
+        DataFrames. Write a graphml file representing the graph.
 
-		Parameters
-		----------
-		searchColumns : list-like
-			Column labels in the sources DataFrame which contain data
-			for the ADS search queries.
+        Parameters
+        ----------
+        name : string
+            Network name will be the prefix for all stored filenames.
+        '''
 
-		fetchTerms : list-like
-			ADS search terms to get for each query. This is a list of
-			data to fields to download from the API rather than search
-			terms with which to construct the search query from the
-			sources	DataFrame. List of ADS terms is in the drop-down
-			menu above the search bar at
-			https://ui.adsabs.harvard.edu/
+        backup(name + '-bib.json')
+        self.bib.to_json(name + '-bib.json')
 
-		kwargs
-			Keyword arguments are passed directly to
-			bibliograph.nasaads.queryADSbibcodes
+        backup(name + '-cit.json')
+        self.bib.to_json(name + '-cit.json')
 
-		Returns
-		-------
-		queries : pd.DataFrame
-			pandas DataFrame with query strings, ads articles objects 
-			retreived from the ADS, and bibcodes from those articles
-			objects. queries index corresponds to sources index.
+        if self.graph:
+            backup(name + '.graphml')
+            nx.write_graphml(self.graph, name + '.graphml')
 
-		badQueries : list
-			List of index values from the sources DataFrame for which
-			values in columns to be searched either contained spaces
-			or were 'x'.	
-		'''
-		results, queries, badQueries = queryADS(self.bib, searchColumns, fetchTerms, **kwargs)
+    def get_ads_bibcodes(self, search_cols, **kwargs):
+        '''
+        Get ADS bibcodes for papers in the sources DataFrame. If this
+        function tries to construct a query for some entry but can't
+        (the index for that entry winds up in badQueries), the
+        bibcode field for that entry gets '?'.
 
-		uid = self.uid
+        Parameters
+        ----------
+        search_cols : list-like
+            Column labels in the sources DataFrame which contain data
+            for the ADS search queries.
 
-		for i in results.index:
+        kwargs
+            Keyword arguments are passed directly to
+            bibliograph.nasaads.query_ads_bibcodes
 
-			srcID = results.loc[i, 'srcidx']
-			
-			thisResult = results.loc[i, results.columns[:-1]].squeeze().copy()
+        Returns
+        -------
+        queries : pd.DataFrame
+            pandas DataFrame with query strings, ads articles objects 
+            retreived from the ADS, and bibcodes from those articles
+            objects. queries index corresponds to sources index.
 
-			if uid == 'ref':
-				thisResult[uid] = ' '.join([thisResult[c] for c in self.refcols if (thisResult[c] != 'x')])
+        badqueries : list
+            List of index values from the sources DataFrame for which
+            values in columns to be searched either contained spaces
+            or were 'x'.    
+        '''
 
-			self.update(thisResult, updateCit=True, src=srcID)
+        queries, badqueries = query_ads_bibcodes(self.bib, search_cols, **kwargs)
 
-		return(results, queries, badQueries)
+        self.bib.loc[queries.index, 'bibcode'] = queries['bibcode']
+        self.bib.loc[badqueries, 'bibcode'] = '?'
+
+        return(queries, badqueries)
+
+    def queryads(self, search_cols, fetch_terms, **kwargs):
+        '''
+        Get ADS data for bibliography entries.
+
+        Parameters
+        ----------
+        search_cols : list-like
+            Column labels in the sources DataFrame which contain data
+            for the ADS search queries.
+
+        fetch_terms : list-like
+            ADS search terms to get for each query. This is a list of
+            data to fields to download from the API rather than search
+            terms with which to construct the search query from the
+            sources DataFrame. List of ADS terms is in the drop-down
+            menu above the search bar at
+            https://ui.adsabs.harvard.edu/
+
+        kwargs
+            Keyword arguments are passed directly to
+            bibliograph.nasaads.queryADSbibcodes
+
+        Returns
+        -------
+        queries : pd.DataFrame
+            pandas DataFrame with query strings, ads articles objects 
+            retreived from the ADS, and bibcodes from those articles
+            objects. queries index corresponds to sources index.
+
+        badqueries : list
+            List of index values from the sources DataFrame for which
+            values in columns to be searched either contained spaces
+            or were 'x'.    
+        '''
+
+        results, queries, badqueries = queryads(self.bib, search_cols, fetch_terms, **kwargs)
+
+        this_updater = (lambda x: self.update_entry(x, update_citations=True, src=x['srcidx']))
+
+        results[results.columns[:-1]].apply(this_updater, axis=1)
+
+        return(results, queries, badqueries)
+
